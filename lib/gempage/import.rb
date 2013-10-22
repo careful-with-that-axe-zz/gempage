f4a29cac55a1b8c5bb79535af07c3140bfa9847d
Jane Sebastian <jane@betatetra.com>
2013-10-22 13:02:06 -0700

    Refactoring to remove unnecessary arrays/hashes

--- lib/gempage/import.rb
+++ lib/gempage/import.rb
@@ -11,28 +11,20 @@ module Gempage::Import
       @gem_list = gem_list
     end
 
-    def gemfile_lines
-      File.readlines(@gemfile_path) if File.exists? @gemfile_path
-    end
-
     def gem_list
       if gemfile_lines
         gems = process_gems(normalize(gemfile_lines))
-        gem_array = []
-        gems.each do |group, values|
-          values.each do |t,r|
-            gem_name = t.keys.first
-            gem_config = t.values.first
-            group_name = group.strip if group.is_a? String
-            group_name = group.join(', ') if group.is_a? Array
-            gem_info = create_rubygem(gem_name, group_name, gem_config)
-            gem_array << gem_info if gem_info
-          end
+        final_gems = []
+        gems.each do |gem_listing|
+          final_gems << add_rubygem_content(gem_listing) if find_gem(gem_listing[:name])
         end
-        gem_array
       else
         puts "There is no Gemfile to process... how odd."
       end
+    end
+
+    def gemfile_lines
+      File.readlines(@gemfile_path) if File.exists? @gemfile_path
     end
 
     def find_gem(name)
@@ -46,25 +38,21 @@ module Gempage::Import
       response.code.to_i == 200 ? response.body : false
     end
 
-    def create_rubygem(name, group, gem_config)
-      rubygem = find_gem(name)
+    def add_rubygem_content(gem_listing)
+      rubygem = find_gem(gem_listing[:name])
       if rubygem
-        rubygem_hash = {
-          name: name,
-          category: group,
-          configuration: gem_config,
-          downloads: rubygem['downloads'],
-          version: rubygem['version'],
-          authors: rubygem['authors'],
-          info: rubygem['info'],
-          documentation_uri: get_documentation_uri(rubygem['documentation_uri'], rubygem['homepage_uri']),
-          source_code_uri: get_source_code_uri(rubygem['source_code_uri'], rubygem['homepage_uri']),
-          homepage_uri: get_homepage_uri(rubygem['homepage_uri'])
-        }
+        gem_listing[:downloads] = rubygem['downloads']
+        gem_listing[:version] = rubygem['version']
+        gem_listing[:authors] = rubygem['authors']
+        gem_listing[:info] = rubygem['info']
+        gem_listing[:documentation_uri] = get_documentation_uri(rubygem['documentation_uri'], rubygem['homepage_uri'])
+        gem_listing[:source_code_uri] = get_source_code_uri(rubygem['source_code_uri'], rubygem['homepage_uri'])
+        gem_listing[:homepage_uri] = get_homepage_uri(rubygem['homepage_uri'])
+        gem_listing
       else
         puts "None such #{name} gem... #{name} will not be in the Gem Reference"
+        false
       end
-      rubygem_hash
     end
 
     def get_homepage_uri(homepage_uri)
@@ -93,16 +81,20 @@ module Gempage::Import
     end
 
     def add_gem(gems, section, line)
-      gems[section] = [] unless gems[section]
-      gems[section] << gem_details(line)
+      gems << gem_details(line, section) if gem_details(line, section)
     end
 
-    def gem_details(line)
+    def gem_details(line, section)
       gem_pieces = line.match(/^gem\s'([\w|-]+)',?\s?(.*)/)
-      gem_pieces ? { gem_pieces[1] => gem_pieces[2] } : nil
+      gem_pieces ? { name: gem_pieces[1], category: format_category(section), configuration: gem_pieces[2] } : nil
     end
 
-    def process_gems(data, section = 'all', gems = {})
+    def format_category(category)
+      return category.strip if category.is_a? String
+      return category.join(', ') if category.is_a? Array
+    end
+
+    def process_gems(data, section = 'all', gems = [])
       data.each_with_index do |line, index|
         if line.match(/^gem\s/)
           add_gem(gems, section, line)
