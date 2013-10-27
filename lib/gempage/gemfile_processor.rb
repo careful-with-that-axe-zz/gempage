0a5f5eef0952cca67be24176952ae8d4c14be84e
Jane Sebastian <jane@betatetra.com>
2013-10-27 10:11:19 -0700

    Fix bug with gem_pieces and set to return false if no gemfile

--- lib/gempage/gemfile_processor.rb
+++ lib/gempage/gemfile_processor.rb
@@ -10,21 +10,22 @@ module Gempage
     end
 
     def gem_list
-      process_gems(normalize(gemfile_lines))
+      process_gems(normalize_lines) if gemfile_lines
     end
 
     private
 
     def gemfile_lines
-      File.readlines(@gemfile_path) if File.exists? @gemfile_path
+      return @gemfile_lines if defined? @gemfile_lines
+      @gemfile_lines = File.exists?(@gemfile_path) ? File.readlines(@gemfile_path) : false
     end
 
-    def normalize(gemfile)
+    def normalize_lines
       # Strip white space, keep only lines starting with gem, group
       # or end, normalize to single quotes, remove trailing comments
-      gemfile.map { |x| x.strip.gsub(/"/,"'") }
-             .reject { |x| x.empty? || !x.match(/^(gem|group|end)/) }
-             .map { |x| x.gsub(/\s?#.*$/, '') }
+      gemfile_lines.map { |x| x.strip.gsub(/"/,"'") }
+                   .reject { |x| x.empty? || !x.match(/^(gem|group|end)/) }
+                   .map { |x| x.gsub(/\s?#.*$/, '') }
     end
 
     # The heart of the system that parses through the Gemfile
@@ -49,7 +50,7 @@ module Gempage
 
     def gem_details(line, group)
       gem_pieces = line.match(/^gem\s'([\w|-]+)',?\s?(.*)/)
-      configuration = gem_pieces[1] if gem_pieces[1] && gem_pieces[1].strip.length != 0
+      configuration = gem_pieces[2] && gem_pieces[2].strip.length != 0 ? gem_pieces[2] : nil
       gem_pieces ? { 'name' => gem_pieces[1], 'category' => group, 'configuration' => configuration } : nil
     end
 
