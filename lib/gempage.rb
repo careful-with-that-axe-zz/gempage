3883eb915910f8955df28837761fdcfc5ccd5c60
Jane Sebastian <jane@betatetra.com>
2013-10-28 17:33:58 -0700

    Removing unnecessary parameter being passed about

--- lib/gempage.rb
+++ lib/gempage.rb
@@ -11,12 +11,12 @@ module Gempage
   class << self
 
     def generate(options = nil)
-      result ? format(result, options) : puts(output_message(result))
+      result ? format(options) : puts(output_message)
     end
 
     def result
       return @result if defined? @result
-      @result = gem_listing ? merge_gem_and_info(gem_listing) : false
+      @result = gem_listing ? merge_gem_and_info : false
     end
 
     def gem_listing
@@ -24,20 +24,20 @@ module Gempage
       @gem_listing = Gempage::GemfileProcessor.new(gemfile_path).gem_list
     end
 
-    def merge_gem_and_info(gem_listing)
+    def merge_gem_and_info
       gem_listing.each_with_index do |listed_gem, index|
         ruby_gem_json = Gempage::RubyGemInfo.new(listed_gem['name']).gem_json
         gem_listing[index].merge!(ruby_gem_json)
       end
     end
 
-    def format(result, options)
-      create_stylesheet('application', options)
-      create_index(result)
-      puts output_message(result)
+    def format(options)
+      create_stylesheet(options)
+      create_index
+      puts output_message
     end
 
-    def create_index(result)
+    def create_index
       File.open(File.join(gempage_path, "index.html"), "w+") do |file|
         file.puts template('layout').result(binding)
       end
@@ -47,7 +47,7 @@ module Gempage
       ERB.new(File.read(File.join(File.dirname(__FILE__), '../views/', "#{name}.erb")))
     end
 
-    def create_stylesheet(name, options)
+    def create_stylesheet(options)
       File.open(File.join(asset_gempage_path, "application.css"), "w+") do |file|
         file.puts File.readlines(File.join(File.dirname(__FILE__), "../public/application.css"))
         file.puts File.readlines(File.join(File.dirname(__FILE__), "../public/color-schemes/#{color_scheme_file(options)}.css"))
@@ -62,7 +62,7 @@ module Gempage
       File.exist?(File.join(File.dirname(__FILE__), "../public/color-schemes/#{file_name}.css"))
     end
 
-    def output_message(result)
+    def output_message
       if result
         category_count = result.group_by{ |result| result['category'] }.length
         "Gem list generated for #{category_count} gem groups and #{result.length} gems to #{gempage_path}."
